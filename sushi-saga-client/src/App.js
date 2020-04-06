fb4f2382fdf3108ba951d21e79fb4dc916983339
Granit <ghaxhaj@fordham.edu>
2020-04-06 14:21:29 -0400

    Wtf

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,34 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: {sushis: []},
+    clickedSushi: []
+  }
+
+  componentDidMount(){
+    fetch(`${API}`)
+    .then(resp => resp.json())
+    .then(data => this.setState({sushis: data}))
+  }
+
+  getClickedSushi = (id) => {
+    let clickedSushi = [...this.state.clickedSushi]
+    let tester = clickedSushi.filter(sushi => sushi.id === id)
+    let sushi = [...this.state.sushis]
+    let selectedSushi = sushi.filter(sushi => sushi.id === id)
+    if(tester.length > 0){
+      console.log("You already Ate this Sushi")
+    }else{
+    this.setState({clickedSushi: [...this.state.clickedSushi, selectedSushi]})
+  }}
+
+
   render() {
+    console.log(this.state.clickedSushi)
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer sushi = {this.state.sushis} handleClick = {this.getClickedSushi}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,42 @@
 import React, { Fragment } from 'react'
+import Sushi from "../components/Sushi"
 import MoreButton from '../components/MoreButton'
 
-const SushiContainer = (props) => {
+class SushiContainer extends React.Component{
+
+  state = {
+    firstLimit: 0,
+    lastLimit: 4
+  }
+
+  renderSushi = () => {
+    if(this.props.sushi.length > 0){
+      var fourSushi = this.props.sushi.slice(this.state.firstLimit,this.state.lastLimit)
+      return fourSushi.map(sushi => {
+        return <Sushi key={sushi.id} handleClick = {this.props.handleClick} {...sushi} />
+      })
+    }
+  }
+
+  onLoadMore = () => {
+    this.setState({
+      firstLimit: this.state.firstLimit + 4,
+      lastLimit: this.state.lastLimit + 4
+    })
+  }
+
+  render(){
+
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          this.renderSushi()
         }
-        <MoreButton />
+        <MoreButton handleClick = {this.onLoadMore}/>
       </div>
     </Fragment>
-  )
+  )}
 }
 
 export default SushiContainer
